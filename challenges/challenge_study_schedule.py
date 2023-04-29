9d8d143d506d1b3a17b181604e253ad5e3b2de0b
RafaelRRhocha <1rafaelrocha2@gmail.com>
2023-04-29 13:16:11 -0300

    feat: study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(per, tm):
+    set_count = 0
+    if not tm:
+        return None
+    for start, out in per:
+        if not isinstance(start, int) or not isinstance(out, int):
+            return None
+        if start <= tm <= out:
+            set_count += 1
+    return set_count
+
